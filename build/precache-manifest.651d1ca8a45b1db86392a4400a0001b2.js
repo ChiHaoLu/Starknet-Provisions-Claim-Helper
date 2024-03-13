24cbb76bf6192e0514cf84a71493f2cfa2de2ec7
ChiHaoLu <b08303113@ntu.edu.tw>
2024-03-13 19:20:00 +0800

    add build

---
+++ build/precache-manifest.651d1ca8a45b1db86392a4400a0001b2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507aaacc4ff9d2d8b11fa449f9e86fcf",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/index.html"
+  },
+  {
+    "revision": "f075eccc8cf70e77daed",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "6a1dc3f79f0479e778e5",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/css/main.498f1462.chunk.css"
+  },
+  {
+    "revision": "5fb52ff3121ffce2d34b",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/10.b7b64951.chunk.js"
+  },
+  {
+    "revision": "5feb35fb501d4ede4fc7",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/11.307139e3.chunk.js"
+  },
+  {
+    "revision": "53631bb8bc0252d16fef",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/12.5e93d836.chunk.js"
+  },
+  {
+    "revision": "abe58b8dc157b81394b3",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/13.c063a8b8.chunk.js"
+  },
+  {
+    "revision": "f075eccc8cf70e77daed",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/2.a2648f32.chunk.js"
+  },
+  {
+    "revision": "6cad498bb896eb08cf7a",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/3.ea416ca8.chunk.js"
+  },
+  {
+    "revision": "d2c782721553dddf1308",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/4.91e6087e.chunk.js"
+  },
+  {
+    "revision": "f882df20e1323d53bc23",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/5.6ded1d58.chunk.js"
+  },
+  {
+    "revision": "d42a89f90d3646c64950",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/6.c56c0372.chunk.js"
+  },
+  {
+    "revision": "826f111e29b575c46370",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/7.81a864a1.chunk.js"
+  },
+  {
+    "revision": "27c4d3355edaf8cdb033",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/8.07fd85c3.chunk.js"
+  },
+  {
+    "revision": "68d6f85cdac76d91a37d",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/9.37184ffb.chunk.js"
+  },
+  {
+    "revision": "6a1dc3f79f0479e778e5",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/main.4a93369c.chunk.js"
+  },
+  {
+    "revision": "3888dbed0c1595d936e3",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/js/runtime~main.fbe51ef9.js"
+  },
+  {
+    "revision": "62ac9f0e2c22d406a68c0430d009eccd",
+    "url": "/ChiHaoLu/Starknet-Provisions-Claim-Helper/static/media/Starkscan.62ac9f0e.png"
+  }
+]);
